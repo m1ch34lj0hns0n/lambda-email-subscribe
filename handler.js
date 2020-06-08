043150062a6d079d7a0a5c591ad6c5ae23ad16b0
Michael Johnson <michael.ben.johnson@gmail.com>
2020-06-09 00:06:08 +0100

    Add mailchimp wrapper, apis, bunch of other stuff

--- handler.js
+++ handler.js
@@ -1,18 +1,46 @@
 'use strict';
 
-module.exports.hello = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+require('dotenv');
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+const Mailchimp = require('mailchimp-api-v3');
+
+const API_KEY  = process.env.API_KEY
+const LIST_ID  = process.env.LIST_ID;
+
+const subscribe = async email => {
+  const mailChimp = new Mailchimp(API_KEY);
+
+  return mailChimp
+    .request({
+      method: 'post',
+      path: '/lists/' + LIST_ID + '/members',
+      body: {
+        email_address: email,
+        status: 'subscribed'
+      }
+    })
+    .then(response => response)
+    .catch(error => error);
+};
+
+module.exports.subscribe = async event => {
+  let response = null;
+  const body = event.body;
+  const request = await subscribe(body.email);
+  
+  if (request.statusCode === 200) {
+    return {
+      statusCode: request.statusCode,
+      body: JSON.stringify({
+        message: 'subscribed'
+      })
+    };
+  } else {
+    return {
+      statusCode: request.status,
+      body: JSON.stringify({
+        message: request.title,
+      })
+    };
+  }
 };
